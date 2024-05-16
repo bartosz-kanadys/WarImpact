50471d9da67c188a0a49bc32506fe9617c606bf4
Bartosz Kanadys <kanadysbartosz@gmail.com>
2024-05-16 21:48:36 +0200

    update oil api

--- server/routes/oil.js
+++ server/routes/oil.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-//var sequelize = require('../database/sequelize')
 var Oil = require('../database/models/OilModel')
 
 var url = 'https://www.alphavantage.co/query?function=BRENT&interval=monthly&apikey=demo'
 
-router.get('/getOilFromAPI', function (req, res, next) {
+router.get('/getFromAPI', function (req, res, next) {
   fetch(url)
     .then(res => res.json())
     .then(json => {
@@ -18,26 +17,47 @@ router.get('/getOilFromAPI', function (req, res, next) {
           }
         );
       });
-
-     // res.json(json.data)
      res.send("Do bazy danych dodano historie cen baryłki ropy od roku 1987")
-
     })
-
 });
 
-router.get('/getAllOils', function(req, res) {
+router.get('/getPrice/:year/:month/', function(req, res) {
+  var date = req.params.year+"-"+req.params.month+"-01"
+  console.log(date)
+  Oil.findOne({
+    where:{date: date},
+  }) 
+  .then(data => res.json(data))
+  .catch(err => {
+    console.error('Błąd w trakcie pobierania danych:', err);
+    res.status(500).json({ error: 'Wystąpił błąd podczas pobierania danych.' });
+  });
+})
+
+router.get('/getPrice/:year/:month/:day', function(req, res) {
+  var date = req.params.year+"-"+req.params.month+"-"+req.params.day
+  console.log(date)
+  Oil.findOne({
+    where:{date: date},
+  }) 
+  .then(data => res.json(data))
+  .catch(err => {
+    console.error('Błąd w trakcie pobierania danych:', err);
+    res.status(500).json({ error: 'Wystąpił błąd podczas pobierania danych.' });
+  });
+})
+
+router.get('/getAll', function(req, res) {
   Oil.findAll({
     where: {},
     attributes: ['date', 'price'],
   })
     .then(data => res.json(data))
     .catch(err => {
-      console.error('Błąd w trakcie pobierania olejów:', err);
-      res.status(500).json({ error: 'Wystąpił błąd podczas pobierania olejów.' });
+      console.error('Błąd w trakcie pobierania danych:', err);
+      res.status(500).json({ error: 'Wystąpił błąd podczas pobierania danych.' });
     });
 });
-
 
 router.delete("/deleteAll", function(req,res){
   Oil.destroy({
