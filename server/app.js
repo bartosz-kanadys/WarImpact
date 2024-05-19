367284fd2534c6b81d0834c47563892559f42b90
Bartosz Kanadys <kanadysbartosz@gmail.com>
2024-05-19 17:43:01 +0200

    Delete views from server

--- server/app.js
+++ server/app.js
@@ -5,16 +5,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
 
-
 var indexRouter = require('./routes/index');
 var oilRouter = require('./routes/resourcesRoute');
 
 var app = express();
-
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -25,12 +19,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/res', oilRouter);
 app.use(cors);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
-
 
 // error handler
 app.use(function(err, req, res, next) {
