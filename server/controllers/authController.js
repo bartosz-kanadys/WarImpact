403f936d0c68522cc2eb1d93d56702b1e8430e56
Bartosz Kanadys <kanadysbartosz@gmail.com>
2024-05-21 20:18:17 +0200

    Add isolation level SERIALIZABLE

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -8,6 +8,7 @@ dotenv.config()
 var User = require('../database/models/UserModel')
 var roles = require('../roles');
 const sequelize = require('../database/sequelize');
+const { Transaction } = require('sequelize');
 
 
 module.exports = {
@@ -36,49 +37,53 @@ module.exports = {
 
     async register(req, res, next) {
         try {
-            await sequelize.transaction(async () => {
-                const user = req.body
-                const { login, email, password, role } = user
+            await sequelize.transaction(
+                {
+                    isolationLevel: Transaction.ISOLATION_LEVELS.SERIALIZABLE
+                },
+                async () => {
+                    const user = req.body
+                    const { login, email, password, role } = user
 
-                const isEmailExist = await User.findOne({ where: { email: email } })
-                const isLoginExist = await User.findOne({ where: { login: login } })
+                    const isEmailExist = await User.findOne({ where: { email: email } })
+                    const isLoginExist = await User.findOne({ where: { login: login } })
 
-                if (isEmailExist) {
-                    res.status(400).json({
-                        status: 400,
-                        message: "Email istnieje już w bazie",
+                    if (isEmailExist) {
+                        res.status(400).json({
+                            status: 400,
+                            message: "Email istnieje już w bazie",
+                        });
+                        return;
+                    }
+
+                    if (isLoginExist) {
+                        res.status(400).json({
+                            status: 400,
+                            message: "Login istnieje już w bazie",
+                        });
+                        return;
+                    }
+
+                    const saltRounds = 10;
+
+                    bcrypt.genSalt(saltRounds, function (err, salt) {
+                        bcrypt.hash(password, salt, function (err, hash) {
+                            if (err) throw err;
+                            User.create({
+                                login: login,
+                                email: email,
+                                password: hash,
+                                role: role
+                            })
+                        });
                     });
-                    return;
-                }
 
-                if (isLoginExist) {
-                    res.status(400).json({
-                        status: 400,
-                        message: "Login istnieje już w bazie",
+                    res.status(200).json({
+                        status: 201,
+                        success: true,
+                        message: "Zarejestrowano użytkownika"
                     });
-                    return;
-                }
-
-                const saltRounds = 10;
-
-                bcrypt.genSalt(saltRounds, function (err, salt) {
-                    bcrypt.hash(password, salt, function (err, hash) {
-                        if (err) throw err;
-                        User.create({
-                            login: login,
-                            email: email,
-                            password: hash,
-                            role: role
-                        })
-                    });
-                });
-
-                res.status(200).json({
-                    status: 201,
-                    success: true,
-                    message: "Zarejestrowano użytkownika"
-                });
-            })
+                })
         } catch (error) {
             res.status(400).json({
                 status: 400,
