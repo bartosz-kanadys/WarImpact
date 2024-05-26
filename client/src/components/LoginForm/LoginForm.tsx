a48ad2c3fe969f0630ea607bd8389b705d02a9f7
Jzerior <jakubjezior@gmail.com>
2024-05-26 17:20:06 +0200

    Added navigation menu

--- client/src/components/LoginForm/LoginForm.tsx
+++ client/src/components/LoginForm/LoginForm.tsx
@@ -15,9 +15,9 @@ export const LoginForm = () => {
 
     return (
     <section className="bg-gray-50 dark:bg-gray-900">
-        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
             <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-                <img className="w-8 h-8 mr-2" src="https://emojigraph.org/media/facebook/beaver_1f9ab.png" alt="logo" />
+                <img className="w-16 h-16 mr-2" src="https://emojigraph.org/media/facebook/beaver_1f9ab.png" alt="logo" />
                 Bober
             </a>
             <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
