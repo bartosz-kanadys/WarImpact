d59439c73c8f821e6f3be6b49a526b3672353bca
Jakub Jezior <jakubjezior@gmail.com>
2024-05-28 13:36:55 +0200

    Download page improvement

--- client/src/components/Download/Download.tsx
+++ client/src/components/Download/Download.tsx
@@ -8,11 +8,16 @@ export const Download = () => {
                     <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">JSON files with data we use</h2>
                 </div>
                 <div className="space-y-8 lg:grid lg:grid-cols-5 sm:gap-6 xl:gap-10 lg:space-y-0">
-                    <Card/>
-                    <Card/>
-                    <Card/>
-                    <Card/>
-                    <Card/>
+                    <Card name="Aluminium price history"/>
+                    <Card name="Coffe price history"/>
+                    <Card name="Conflicts 💀 history"/>
+                    <Card name="Copper price history"/>
+                    <Card name="Corn price history"/>
+                    <Card name="Cotton price history"/>
+                    <Card name="Gas price history"/>
+                    <Card name="Oil price history"/>
+                    <Card name="Sugar price history"/>
+                    <Card name="Wheat price history"/>
                 </div>
             </div>
         </section>
